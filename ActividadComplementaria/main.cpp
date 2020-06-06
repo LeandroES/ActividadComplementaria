11b3405dd3ea1ab9f051b86f7b80b8a4c5797789
Leandro Igor Estrada Santos <lestradas@ulasalle.edu.pe>
2020-06-05 20:52:18 -0500

    Completo sin templates, sin contenedores, sin sobrecarga de operadores.

--- ActividadComplementaria/main.cpp
+++ ActividadComplementaria/main.cpp
@@ -14,34 +14,84 @@
 #include <map>
 
 using namespace std;
-int _insert(int a);
+int Insert(int a);
 
-class Device{
-    
+class Device{   //--------> DEVICE
   public:
     double priceDevice;
     int codDevice;
     string descDevice;
-    Device();
-    Device(double, int, string);
-    Device(string, double, int);
+    
     virtual void Print()=0;
     virtual void Work()=0;
     virtual void UseEnergy()=0;
-    virtual void TurnOff();
+    virtual void TurnOff()=0;
 };
 
-class Processor : public Device{
+class Processor : public Device{ //--------> PROCESSOR
   private:
     int genProcessor;
     float GHrz;
   public:
     Processor();
-    Processor(int, float);
-    void Print();
-    void Process();
-    void Read();
-    void Write();
+    
+    
+    Processor(string descDevice, int codDevice, double priceDevice, float GHrz, int genProcessor){
+        
+        this->genProcessor=genProcessor;
+        this->GHrz=GHrz;
+        this->codDevice=codDevice;
+        this->descDevice=descDevice;
+        this->priceDevice=priceDevice;
+        
+    }
+    
+    void Process(){
+
+        cout<<"Processing..."<<endl;
+
+    }
+    void Read(){
+
+        cout<<"Reading..."<<endl;
+
+    }
+
+    void Write(){
+        
+        cout<<"Writing..."<<endl;
+        
+    }
+    
+    virtual void Print(){
+        
+        cout<<"Code: "<<codDevice<<endl;
+        cout<<"Description: "<<descDevice<<endl;
+        cout<<"Price: "<<priceDevice<<endl;
+        cout<<"Processor generation: "<<genProcessor<<endl;
+        cout<<"Speed in GHz: "<<GHrz<<endl;
+        
+    }
+    
+    virtual void Work(){
+
+      cout<<"It works..."<<endl;
+
+    }
+    
+    virtual void UseEnergy(){
+
+      cout<<"Using significant energy..."<<endl;
+
+    }
+    
+    virtual void TurnOff(){
+
+      cout<<"Turning off..."<<endl;
+
+    }
+    
+    
     
 };
 
@@ -52,174 +102,118 @@ class Smartphone : public Device{
     string Gamma;
   public:
     Smartphone();
-    Smartphone(int, string, string);
-    void Print();
-    void Call();
-    void Write();
-    void TakePhotos();
-    void Charge();
+    Smartphone(string descDevice, int codDevice, double priceDevice, int numSerieS, string specsS, string Gamma){
+        
+        this->Gamma=Gamma;
+        this->numSerieS=numSerieS;
+        this->specsS=specsS;
+        this->codDevice=codDevice;
+        this->descDevice=descDevice;
+        this->priceDevice=priceDevice;
+
+    }
+    Smartphone(string descDevice, int codDevice, double priceDevice, string specsS, int numSerieS, string Gamma){
+      
+        this->Gamma=Gamma;
+        this->numSerieS=numSerieS;
+        this->specsS=specsS;
+        this->codDevice=codDevice;
+        this->descDevice=descDevice;
+        this->priceDevice=priceDevice;
+
+    }
+    Smartphone(string descDevice, int codDevice, double priceDevice, string specsS, string Gamma, int numSerieS){
+      
+        this->Gamma=Gamma;
+        this->numSerieS=numSerieS;
+        this->specsS=specsS;
+        this->codDevice=codDevice;
+        this->descDevice=descDevice;
+        this->priceDevice=priceDevice;
+
+    }
+    
+   void Call(){
+
+      cout<<"Calling..."<<endl;
+
+    }
+    void Write (){
+
+      cout<<"Writing..."<<endl;
+
+    }
+    void TakePhotos(){
+
+      cout<<"Taking pics..."<<endl;
+
+    }
+    void Charge(){
+        
+        cout<<"Charge status: disabling..."<<endl;
+        
+    }
+    
+    virtual void Print() {
+
+        cout<<"Code: "<<codDevice<<endl;
+        cout<<"Description: "<<descDevice<<endl;
+        cout<<"Price: "<<priceDevice<<endl;
+        cout<<"Serial Number: "<<numSerieS<<endl;
+        cout<<"Specifications: "<<specsS<<endl;
+        cout<<"Gamma: "<<Gamma<<endl;
+        
+    }
+    
+    virtual void Work(){
+
+      cout<<"It works..."<<endl;
+
+    }
+    
+    virtual void UseEnergy(){
+
+      cout<<"Using significant energy..."<<endl;
+
+    }
+    
+    virtual void TurnOff(){
+
+      cout<<"Turning off..."<<endl;
+
+    }
       
 };
 
 
-Device::Device(double price,int cod,string Desc){
-
-    
-    this->priceDevice= price;
-    this->codDevice=cod;
-    this->descDevice=Desc;
-    
-    
-}
-void Device::TurnOff(){
-
-  cout<<"Turning off..."<<endl;
-
-}
-void Device::UseEnergy(){
-
-  cout<<"Using significant energy..."<<endl;
-
-}
-
-void Device::Print(){
-  cout<<"Description: "<<descDevice<<endl;
-  cout<<"Code: "<<codDevice<<endl;
-  cout<<"Price: "<<priceDevice<<endl;
-
+template <class T>
+T GetMax (T a, T b) {
+  T result;
+  result = (a>b)? a : b;
+  return (result);
 }
 
 
-
-Processor::Processor(int genProcessor, float GHrz){
-    
-    this->genProcessor=genProcessor;
-    this->GHrz=GHrz;
-    
-}
-void Processor::Process(){
-
-    cout<<"Processing..."<<endl;
-
-}
-void Processor::Read(){
-
-    cout<<"Reading..."<<endl;
-
-}
-
-void Processor::Write(){
-    
-    cout<<"Writing..."<<endl;
-    
-}
-
-void Processor::Print(){
-
-    Device::Print();
-    cout<<"Processor generation: "<<genProcessor<<endl;
-    cout<<"Speed in GHz: "<<GHrz<<endl;
-
-}
-
-Smartphone::Smartphone(int numSerieS, string specsS, string Gamma){
-  
-    this->numSerieS=numSerieS;
-    this->specsS=specsS;
-
-}
-void Smartphone::Call(){
-
-  cout<<"Calling..."<<endl;
-
-}
-void Smartphone::Write (){
-
-  cout<<"Writing..."<<endl;
-
-}
-void Smartphone::TakePhotos(){
-
-  cout<<"Taking pics..."<<endl;
-
-}
-void Smartphone::Print() {
-
-    Device::Print();
-    cout<<"Serial Number: "<<numSerieS<<endl;
-    cout<<"Specifications: "<<specsS<<endl;
-    cout<<"Gamma: "<<Gamma<<endl;
-    
-}
-template<class TipoD >
-void mostrarMayor(TipoD numero);
-
-
-
-
-
-int _insert(int a){
- vector<int> v4{1,2,3,4,5};
-    vector<int> v5{6,7,8,9,10};
-    v4.insert(v4.end(),v5.begin(),v5.begin()+5);
-    for(int i=0;i<v4.size();i++)
-    cout<<v4[i]<<" ";
-    return 0;
- 
-}
-template<class TipoD >
-void mostrarMayor(TipoD numero){
-
-  float maximo[5], max=0;
-     numero=0;
-    printf("Ingrese 5 valores para determinar el numero maximo\n");
- 
-    for (int i = 1; i <= 5; i++){
- 
-        printf("valor %d :\n",i);
-        scanf("%f",&maximo[i]);
- 
-        if(maximo[i] > max){
-            max = maximo[i];
-            numero = i;
-        }
- 
-    }
- 
-    printf("El numero maximo es: %.2f\n y fue el numero %d en ser ingresado",max,numero);
-
-}
 
 int main() {
-  int v;
-
-  Device *vector[3];
-    vector[0] = new Processor(15,456128995,"Adolfo",86521,12.5,2);
-
     
-    vector[0]->Print();
-    printf("/r/n");
-    cout<<endl<<endl;
-
-
-  vector[0] = new Profesor(35,73945202,"Maria",23546,25601.50,"Matematica");
-
-  vector[0]->mostrar();
-
-  cout<<"--------------------------------"<<endl;
-  
-  vector[0] = new Persona("Gustavo",73947504,20);
-
-  vector[0]->mostrar();
-   cout<<"--------------------------------"<<endl;
-
-  _insert(v);
-
-  cout<<"--------------------------------"<<endl;
-  
-    mostrarMayor(10);
-
-  system ("pause");
-  return 0;
+    Processor p1("Intel i7",            123123, 1700.99,    10, 3.1);
+    Processor p2("Intel i5",            134131, 1640.99,    8,  3.6);
+    Processor p3("Intel i5",            252452, 1200.99,    7,  2.8);
+    Processor p4("Intel i7",            467467, 1800.99,    9,  2.8);
+    Processor p5("Celeron Dual core",   689689, 900.99,     3,  1.5);
+    Processor p6("Intel i9",            981383, 3400.99,    10, 5.1);
+    
+    Smartphone s1("iPhone 6",       245245, 1300.99, 12345689, "Black Space",   "Baja");
+    Smartphone s2("iPhone 6S",      245245, 1300.99, 12345689, "Gray",          "Baja");
+    Smartphone s3("iPhone 6S Plus", 245245, 1300.99, 12345689, "black",         "Media");
+    Smartphone s4("iPhone X",       245245, 1300.99, 12345689, "Blue Space",    "Alta");
+    Smartphone s5("iPhone XS",      245245, 1300.99, 12345689, "Veige Space",   "Alta");
+    Smartphone s6("iPhone SE",      245245, 1300.99, 12345689, "Fuchsia Space", "Baja");
+    
+    
+    
+    
+    return 0;
   
 }
